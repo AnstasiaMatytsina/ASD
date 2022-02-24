d3f2c6616008d68d234e04c1d657b2e19af26a10
Anastasia Matytsina <matytsina.nastenka@mail.ru>
2022-02-24 14:40:52 +0700

    Создание проекта sort1

---
+++ sem2/Sort1/Sort1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+
+
+int createAndSortFile(const std::string& fileName, const int numbersCount, const int maxNumberValue)
+{
+    if (!createFileWithRandomNumbers(fileName, numbersCount, maxNumberValue)) {
+        return -1;
+    }
+
+    sortFile(fileName); //Вызов вашей функции сортировки
+
+    if (!isFileContainsSortedArray(fileName)) {
+        return -2;
+    }
+
+    return 1;
+}
+
+bool createFileWithRandomNumbers(const std::string& fileName, const int numbersCount, const int maxNumberValue)
+{
+    return 1;
+}
+bool isFileContainsSortedArray(const std::string& fileName)
+{
+    return 1;
+}
+void sortFile(const std::string& fileName)
+{
+
+}
+int main()
+{
+    std::string fileName = "file.txt";
+    const int numbersCount = 1000000;
+    const int maxNumberValue = 100000;
+
+    for (int i = 0; i < 10; i++) {
+        switch (createAndSortFile(fileName, numbersCount, maxNumberValue)) {
+        case 1:
+            std::cout << "Test passed." << std::endl;
+            break;
+
+        case -1:
+            std::cout << "Test failed: can't create file." << std::endl;
+            break;
+
+        case -2:
+            std::cout << "Test failed: file isn't sorted." << std::endl;
+            break;
+        }
+        return 0;
+    }
+}
